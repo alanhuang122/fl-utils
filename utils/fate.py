ae0b99d45cdda9fc3240feddc703ab988154daff
Alan Huang <Alan.Huang@utdallas.edu>
2018-07-23 15:18:34 -0500

    improve compatability

--- utils/fate.py
+++ utils/fate.py
@@ -1,24 +1,24 @@
+#!/usr/bin/env python3
 from bs4 import BeautifulSoup as Soup
 import netrc
 from operator import itemgetter
 import re
 import requests
 
-creds = netrc.netrc()
+target_currency = 'USD'
 
+creds = netrc.netrc()
 fl = creds.authenticators('fallenlondon')
 fixer = creds.authenticators('fixer.io')
 
 s = requests.Session()
-
 r = s.post('http://fallenlondon.storynexus.com/Auth/EmailLogin', data={'emailAddress': fl[0], 'password': fl[2]})
 r = s.get('http://fallenlondon.storynexus.com/sn/BuyNexBraintree')
 
 soup = Soup(r.text, 'lxml')
-
 select = soup.find('select', id='currency-code')
 currencies = [option.text for option in select.children]
-target_currency = 'USD'
+
 if target_currency not in currencies:
     r = requests.get('http://data.fixer.io/api/latest', params={'access_key': fixer[0], 'symbols': ','.join(currencies + [target_currency])}).json()
 else:
@@ -26,9 +26,8 @@ else:
 rates = {k: 1 / r['rates'][target_currency] * v for k, v in r['rates'].items()}
 
 fate_rates = []
-
 for currency in currencies:
-    page = s.get(f'https://fallenlondon.storynexus.com/sn/BuyNexBraintree?currencyCode={currency}')
+    page = s.get('https://fallenlondon.storynexus.com/sn/BuyNexBraintree?currencyCode={}'.format(currency))
     soup = Soup(page.text, 'lxml')
     nums = soup.find('ul')
     cur_rates = [tag.text.strip() for tag in nums.children if not isinstance(tag, str)]
@@ -37,10 +36,10 @@ for currency in currencies:
         fate = int(m.group(1))
         cost = float(m.group(2))
         fate_rates.append((currency, fate, cost, fate/cost * rates[currency]))
-
 fate_rates.sort(key=itemgetter(3), reverse=True)
+
 print('The most favorable currencies (ignoring forex fees) are:')
 for rate in fate_rates:
     if rate[0] in currencies:
-        print(f'{rate[0]}: {rate[1]} for {rate[2]} {rate[0]} ({rate[3]:.3f} Fate per {target_currency})')
+        print('{rate[0]}: {rate[1]} for {rate[2]} {rate[0]} ({rate[3]:.3f} Fate per {0})'.format(target_currency, rate=rate))
         currencies.remove(rate[0])
