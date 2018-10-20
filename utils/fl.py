e8bbf501464814805aeb7fcd6b39715f364c028c
Alan Huang <Alan.Huang@utdallas.edu>
2018-10-19 19:22:26 -0500

    Fix missing HTML unescapes

--- utils/fl.py
+++ utils/fl.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python2
 # -*- coding: utf-8 -*-
 import json, re
-from html.parser import HTMLParser
+from html import unescape
 cache = {}
 data = {}
 categories = {0:        'Unspecified',
@@ -75,8 +75,6 @@ categories = {0:        'Unspecified',
               70003:    'Sustenance'
 }
 
-parser = HTMLParser()
-
 def render_html(string):
     string = re.sub(r'<.{,2}?br.{,2}?>','\n', string)
     string = re.sub(r'<.{,2}?[pP].{,2}?>','', string)
@@ -90,24 +88,24 @@ class Quality:
     def __init__(self, jdata):
         #HimbleLevel is used to determine order within categories for items
         self.raw = jdata
-        self.name = jdata.get('Name', '(no name)')
+        self.name = unescape(jdata.get('Name', '(no name)'))
         self.id = jdata['Id']
-        self.desc = jdata.get('Description', '(no description)')
+        self.desc = unescape(jdata.get('Description', '(no description)'))
         self.pyramid = 'UsePyramidNumbers' in jdata
         self.nature = jdata.get('Nature', 1) #1: quality; 2: item
         try:
             qldstr = jdata['ChangeDescriptionText']
-            self.changedesc = parse_qlds(qldstr)
+            self.changedesc = parse_qlds(unescape(qldstr))
         except KeyError:
             self.changedesc = None
         try:
             qldstr = jdata['LevelDescriptionText']
-            self.leveldesc = parse_qlds(qldstr)
+            self.leveldesc = parse_qlds(unescape(qldstr))
         except KeyError:
             self.leveldesc = None
         try:
             variables = {}
-            d = json.loads(jdata['VariableDescriptionText'])
+            d = json.loads(unescape(jdata['VariableDescriptionText']))
             for x in list(d.items()):
                 variables[x[0]] = parse_qlds(x[1])
             self.variables = variables
@@ -279,8 +277,8 @@ def render_requirements(rl, fate):
 class Storylet: #done?
     def __init__(self, jdata, shallow=False):
         self.raw = jdata
-        self.title = parser.unescape(jdata.get('Name', '(no name)'))
-        self.desc = jdata.get('Description', '(no description)')
+        self.title = unescape(jdata.get('Name', '(no name)'))
+        self.desc = unescape(jdata.get('Description', '(no description)'))
         self.id = jdata['Id']
         try:
             self.setting = Setting.get(jdata['Setting']['Id'])
@@ -341,10 +339,10 @@ class Storylet: #done?
 class Branch:   #done
     def __init__(self, jdata, parent):
         self.raw = jdata
-        self.title = parser.unescape(jdata.get('Name', '(no title)'))
+        self.title = unescape(jdata.get('Name', '(no title)'))
         self.id = jdata['Id']
         self.parent = parent
-        self.desc = jdata.get('Description', '(no description)')
+        self.desc = unescape(jdata.get('Description', '(no description)'))
         self.cost = jdata.get('ActionCost', 1)
         self.button = jdata.get('ButtonText', 'Go')
         self.fate = jdata.get('CurrencyCost')
@@ -391,8 +389,8 @@ class Event:    #done
         self.raw = jdata
         self.id = jdata['Id']
         self.parent = None        
-        self.title = parser.unescape(jdata.get('Name', '(no title)'))
-        self.desc = jdata.get('Description', '(no description)')
+        self.title = unescape(jdata.get('Name', '(no title)'))
+        self.desc = unescape(jdata.get('Description', '(no description)'))
         self.category = jdata.get('Category')
         self.effects = []
         if costs:
